dbafd96128c31f5ab4ff02b137dbc2a0c79ce60a
shuheishintani <shuheishintani.dev@gmail.com>
2020-11-23 16:28:30 +0900

    firebase設定用の環境変数を追加

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCkHWK9suodblDkj3hG0aTYOv_dV7ZoAmk',
-  authDomain: 'image-uploader-4c363.firebaseapp.com',
-  databaseURL: 'https://image-uploader-4c363.firebaseio.com',
-  projectId: 'image-uploader-4c363',
-  storageBucket: 'image-uploader-4c363.appspot.com',
-  messagingSenderId: '1020465263722',
-  appId: '1:1020465263722:web:5a2620d4e9f306649a0613',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 if (firebase.apps.length === 0) {
